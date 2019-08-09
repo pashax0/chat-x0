25b849bf2635bc58e7942117b4b1fc8334a57284
Pavel Khomich <pashax0@mail.ru>
2019-08-09 23:23:44 +0300

    Updates

---
+++ precache-manifest.abefb14a2d176423de986198d75d8dbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04fc36468e823f76ee35520ec008009",
+    "url": "/chat-x0/index.html"
+  },
+  {
+    "revision": "178e6bd6c184e5dcf909",
+    "url": "/chat-x0/static/css/2.c37b5d79.chunk.css"
+  },
+  {
+    "revision": "f4c342575649698755a7",
+    "url": "/chat-x0/static/css/3.646ae0ec.chunk.css"
+  },
+  {
+    "revision": "cf1f9e759110c444278a",
+    "url": "/chat-x0/static/css/main.91ecd95d.chunk.css"
+  },
+  {
+    "revision": "178e6bd6c184e5dcf909",
+    "url": "/chat-x0/static/js/2.fd0d55fd.chunk.js"
+  },
+  {
+    "revision": "f4c342575649698755a7",
+    "url": "/chat-x0/static/js/3.4c5bfc50.chunk.js"
+  },
+  {
+    "revision": "cf1f9e759110c444278a",
+    "url": "/chat-x0/static/js/main.d11d7bc9.chunk.js"
+  },
+  {
+    "revision": "48cb66c756585baed4c6",
+    "url": "/chat-x0/static/js/runtime~main.5adf5e0c.js"
+  }
+]);
